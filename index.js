809d7da3b4bde6ed93726dfba5b665c572146f32
Waynee <xiewayne1993@gmail.com>
2018-10-20 01:31:11 -0400

    Finish intro

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+Vue.component("todo-item", {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+});
+
 var app = new Vue({
   el: '#app',
   data: {
-    seen: false
-  }
+    groceryList: [
+      { id: 0, text: 'Vegetable' },
+      { id: 1, text: 'Apple' },
+      { id: 2, text: 'Peach' }
+    ]
+  },
 });

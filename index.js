2ff830d1c1b73319779f1d5de9cea0fa0164b80e
Waynee <xiewayne1993@gmail.com>
2018-10-23 11:19:31 -0400

    finish transition

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-Vue.component('tab-home', {
-  template: '<div>Home component</div>'
-});
-Vue.component('tab-posts', {
-  template: '<div>Posts component</div>'
-});
-Vue.component('tab-archive', {
-  template: '<div>Archive component</div>'
-});
-
 new Vue({
-  el: '#dynamic-component-demo',
+  el: '#example-1',
   data: {
-    currentTab: 'Home',
-    tabs: ['Home', 'Posts', 'Archive']
-  },
-  computed: {
-    currentTabComponent: function () {
-      return 'tab-' + this.currentTab.toLowerCase()
-    }
+    show: true
   }
-});
+})

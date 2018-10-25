6f6e2c01773fb05f907f7fd830d7b56a6bc7b534
Waynee <xiewayne1993@gmail.com>
2018-10-24 22:35:03 -0400

    finish todolist

---
+++ todolist/src/components/BaseInputText.vue
@@ -0,0 +1,27 @@
+<template>
+	<input
+		type="text"
+		class="input"
+		v-model="value"
+		@keydown.enter="$emit('add', value)">
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: ""
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+	@import '../variables.scss';
+
+	.input {
+		width: 100%;
+		padding: 8px 10px;
+		border: 1px solid $vue-blue;
+	}
+</style>
